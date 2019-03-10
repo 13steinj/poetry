120d5162d7a5ad01040e34f6769e20b81153bf8e
13steinj <13steinj@users.noreply.github.com>
2019-03-09 19:24:30 -0500

    Add script for Jython installation

---
+++ travis_jython.sh
@@ -0,0 +1,12 @@
+set -e
+
+pip install jip
+jip install $JYTHON
+NON_GROUP_ID=${JYTHON#*:}
+_JYTHON_BASENAME=${NON_GROUP_ID/:/-}
+OLD_VIRTUAL_ENV=$VIRTUAL_ENV
+java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
+
+virtualenv --version
+# --distribute is a workaround as setuptools don't install on Jython properly
+virtualenv --distribute -p $HOME/jython/bin/jython $HOME/myvirtualenv

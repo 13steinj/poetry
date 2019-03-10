18065ad3bd04a840ddbbbeb6322046a8484c497b
13steinj <13steinj@users.noreply.github.com>
2019-03-10 00:48:45 -0500

    Remove virtual environment

--- travis_jython.sh
+++ travis_jython.sh
@@ -7,6 +7,6 @@ _JYTHON_BASENAME=${NON_GROUP_ID/:/-}
 OLD_VIRTUAL_ENV=$VIRTUAL_ENV
 java -jar $OLD_VIRTUAL_ENV/javalib/${_JYTHON_BASENAME}.jar -s -d $HOME/jython
 
-virtualenv --version
+#virtualenv --version
 # --distribute is a workaround as setuptools don't install on Jython properly
-virtualenv --distribute -p $HOME/jython/bin/jython $HOME/myvirtualenv
+#virtualenv --distribute -p $HOME/jython/bin/jython $HOME/myvirtualenv
